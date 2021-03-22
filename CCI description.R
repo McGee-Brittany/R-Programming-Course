6d38a53c53a4450f574c7a0efc543c0a845f4b53
McGee-Brittany <77584611+McGee-Brittany@users.noreply.github.com>
2021-03-22 01:01:18 -0400

    Add files via upload

---
+++ CCI description.R
@@ -0,0 +1,10 @@
+#Package: CCI McGee
+#Title: The package will Calculate Confidence Intervals (CCI) of a given data set
+#Version: 0.1
+#Authors@R: "Brittany McGee <bmcgee@usf.edu> [aut, cre]"
+#Description: This package will take a given data set and calculate the desired 
+#confidence interval that the user intends (i.e. 95%, 90%, etc.).By doing this 
+# it will return the result in this form (lower bound, upper bound).
+#Depends: R (>= 3.1.2)
+#License: CCO
+#LazyData: true
